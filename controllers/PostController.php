785c3891c01eb6372c37816a1e656b533bb98510
RinaHalili <rina.halili@student.uni-pr.edu>
2024-05-28 23:36:13 +0200

    .

--- controllers/PostController.php
+++ controllers/PostController.php
@@ -12,28 +12,21 @@ class PostController
 
     public function create($data)
     {
-        try {
-            $this->postModel->user_id = $data['user_id'];
-            $this->postModel->title = $data['title'];
-            $this->postModel->content = $data['content'];
+
+        $this->postModel->user_id = $data['user_id'];
+        $this->postModel->title = $data['title'];
+        $this->postModel->content = $data['content'];
 
 
-            if ($this->postModel->create()) {
-                echo "Post created successfully!";
-            } else {
-                echo "Failed to create post!";
-            }
-        } catch (Exception $e) {
-            ErrorHandler::handleExceptions($e);
+        if ($this->postModel->create()) {
+            echo "Post created successfully!";
+        } else {
+            echo "Failed to create post!";
         }
     }
 
     public function getAll()
     {
-        try {
-            return $this->postModel->getAll();
-        } catch (Exception $e) {
-            ErrorHandeler::handleExceptions($e);
-        }
+        return $this->postModel->getAll();
     }
 }
